42178ef984d425b132bcc6b561dd64035cd8edc8
BenLim5878 <limzhengwei1002@gmail.com>
2021-12-25 06:34:00 +0800

    jfx

--- src/main/java/com/ahhasc/Main.java
+++ src/main/java/com/ahhasc/Main.java
@@ -2,24 +2,25 @@ package com.ahhasc;
 import com.ahhasc.Controller.*;
 import com.ahhasc.Model.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
-
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-//        Appointment appointment = new Appointment();
-//        appointment.StartTime = LocalDateTime.now();
-//        appointment.BookingCustomer = new Customer(1);
-//        appointment.ActiveManager = new Manager(0);
-//        appointment.ActiveTechnicians = new ArrayList<Technician>(Arrays.asList(new Technician(0)));
-//        appointment.Payment = new Payment(0);
-//        appointment.setIsCompleted(false);
-//        appointment.setFeedback(new Feedback(0));
-//
-//
-//        DataAccess.GetInstance().AppointmentController.AddAppointment(appointment);
-
+        Application.launch(args);
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
